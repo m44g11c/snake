ba4aca6b5e6e11555d8e8d14c685363a5078f179
m44g11c <abtoputet@tut.by>
2015-10-26 22:42:27 +0300

    Add figure class

---
+++ Snake/Figure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Snake
+{
+	class Figure
+	{
+		
+		protected System.Collections.Generic.List<Point> pList;
+
+		public void Drow ()
+		{
+			foreach (Point p in pList)
+			{
+				p.Draw ();
+			}
+		}
+	}
+}
+
