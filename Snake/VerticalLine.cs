3d3013e45cbb3fa2595b3cd74a2823714d399541
m44g11c <abtoputet@tut.by>
2015-10-26 22:44:35 +0300

    Add vertical line

---
+++ Snake/VerticalLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Snake
+{
+	class VerticalLine : Figure
+	{
+
+		public VerticalLine (int yTop, int yBot, int x, char sym)
+		{
+			pList = new System.Collections.Generic.List<Point> ();
+			for(int y = yTop; y <= yBot; y++)
+			{
+				Point p = new Point(x, y, sym);
+				pList.Add(p);
+			}
+
+		}
+
+	}
+}
+
