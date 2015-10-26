d877e5569bc8b033db083dca6abc00514b91dd8b
m44g11c <abtoputet@tut.by>
2015-10-26 22:44:05 +0300

    Add frame

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,24 +6,17 @@ namespace Snake
 	{
 		public static void Main (string[] args)
 		{
-			Point p1 = new Point(1, 3, '*');
-			p1.Draw();
 
-			Point p2 = new Point (4, 5, '#');
-			p2.Draw ();
 
-			Point p3 = new Point (8, 3, '@');
-			p3.Draw ();
 
-			Point p4 = new Point (2, 4, '!');
-			p4.Draw ();
-
-			System.Collections.Generic.List<Point> pList = new System.Collections.Generic.List<Point>();
-
-			pList.Add (p1);
-			pList.Add (p2);
-			pList.Add (p3);
-			pList.Add (p4);
+			HorizontalLine upLine = new HorizontalLine (0, 78, 0, '#');
+			upLine.Drow ();
+			VerticalLine leftLine = new VerticalLine (0, 23, 0, '#');
+			leftLine.Drow ();
+			HorizontalLine downLine = new HorizontalLine (0, 78, 23, '#');
+			downLine.Drow ();
+			VerticalLine rightLine = new VerticalLine (0, 23, 78, '#');
+			rightLine.Drow ();
 
 			Console.ReadLine ();
 		}
