d3f59189249dcf41cdafc3b02917b370b5689f80
m44g11c <abtoputet@tut.by>
2015-10-26 20:54:39 +0300

    Horizontal line class

---
+++ Snake/HorizontalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Snake
+{
+	class HorizontalLine
+	{
+		System.Collections.Generic.List<Point> pList;
+
+		public HorizontalLine (int xLeft, int xRight, int y, char sym)
+		{
+			pList = new System.Collections.Generic.List<Point> ();
+			for(int x = xLeft; x <= xRight; x++)
+			{
+				Point p = new Point(x, y, sym);
+				pList.Add(p);
+			}
+
+		}
+
+		public void Drow ()
+		{
+			foreach (Point p in pList)
+			{
+				p.Draw ();
+			}
+		}
+
+	}
+}
+
