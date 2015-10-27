7b128990aaff45982067d3d64fd48b283cf829e9
m44g11c <abtoputet@tut.by>
2015-10-27 17:42:04 +0300

    Its moving!

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -19,6 +19,24 @@ namespace Snake
 			Point p = new Point (4, 5, '*');
 			Snake snake = new Snake (p, 4, Direction.RIGHT);
 			snake.Drow ();
+			snake.Move ();
+			System.Threading.Thread.Sleep(300);
+			snake.Move ();
+			System.Threading.Thread.Sleep(300);
+			snake.Move ();
+			System.Threading.Thread.Sleep(300);
+			snake.Move ();
+			System.Threading.Thread.Sleep(300);
+			snake.Move ();
+			System.Threading.Thread.Sleep(300);
+			snake.Move ();
+			System.Threading.Thread.Sleep(300);
+			snake.Move ();
+			System.Threading.Thread.Sleep(300);
+			snake.Move ();
+			System.Threading.Thread.Sleep(300);
+			snake.Move ();
+			System.Threading.Thread.Sleep(300);
 
 			Console.ReadLine ();
 		}
