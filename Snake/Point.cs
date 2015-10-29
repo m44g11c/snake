29725df751e49bec40cd46110733facfdc42e063
m44g11c <abtoputet@tut.by>
2015-10-29 20:58:29 +0300

    Add isHit method

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -24,6 +24,11 @@ namespace Snake
 			sym = p.sym;
 		}
 
+		public bool IsHit(Point p)
+		{
+			return p.x == this.x && p.y == this.y;
+		}
+
 		public void Move(int offset, Direction direction)
 		{
 			if (direction == Direction.RIGHT) {
