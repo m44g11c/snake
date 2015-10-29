e2e41f1113bb35a9392f778e38b46eca194c6e1e
m44g11c <abtoputet@tut.by>
2015-10-29 20:58:53 +0300

    Snake eating

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -20,7 +20,21 @@ namespace Snake
 			Snake snake = new Snake (p, 4, Direction.RIGHT);
 			snake.Drow ();
 
+			FoodCreator foodCreator = new FoodCreator (77, 22, '$');
+			Point food = foodCreator.CreateFood ();
+			food.Draw ();
+
 			while (true) {
+
+				if (snake.Eat (food)) {
+					food = foodCreator.CreateFood ();
+					food.Draw ();
+				} else {
+					snake.Move ();
+				}
+
+				System.Threading.Thread.Sleep(200);
+
 				if(Console.KeyAvailable)
 				{
 					ConsoleKeyInfo key = Console.ReadKey ();
@@ -28,8 +42,6 @@ namespace Snake
 			
 				}
 
-				System.Threading.Thread.Sleep(200);
-				snake.Move ();
 			}
 
 			Console.ReadLine ();
