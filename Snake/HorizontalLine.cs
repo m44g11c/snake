89c026dae3e62302308ccdbc32d23f72ecca4446
m44g11c <abtoputet@tut.by>
2015-10-30 16:02:16 +0300

    Line

--- Snake/HorizontalLine.cs
+++ Snake/HorizontalLine.cs
@@ -1,21 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
 	class HorizontalLine : Figure
 	{
-
-		public HorizontalLine (int xLeft, int xRight, int y, char sym)
+		public HorizontalLine(int xLeft, int xRight, int y, char sym)
 		{
-			pList = new System.Collections.Generic.List<Point> ();
+			pList = new List<Point>();
 			for(int x = xLeft; x <= xRight; x++)
 			{
-				Point p = new Point(x, y, sym);
-				pList.Add(p);
-			}
-
+				Point p = new Point( x, y, sym );
+				pList.Add( p );
+			}			
 		}
-
 	}
 }
 
