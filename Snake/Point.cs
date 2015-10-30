f5c7adbebd9b406e405efa05301ee030cafb3b5e
m44g11c <abtoputet@tut.by>
2015-10-30 16:02:27 +0300

    Point

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -8,13 +8,15 @@ namespace Snake
 		public int y;
 		public char sym;
 
-
-
-		public Point(int _x, int _y, char _sym)
+		public Point()
 		{
-			x = _x;
-			y = _y;
-			sym = _sym;
+		}
+
+		public Point(int x, int y, char sym)
+		{
+			this.x = x;
+			this.y = y;
+			this.sym = sym;
 		}
 
 		public Point(Point p)
@@ -24,35 +26,46 @@ namespace Snake
 			sym = p.sym;
 		}
 
+		public void Move(int offset, Direction direction)
+		{
+			if(direction == Direction.RIGHT)
+			{
+				x = x + offset;
+			}
+			else if(direction == Direction.LEFT)
+			{
+				x = x - offset;
+			}
+			else if(direction == Direction.UP)
+			{
+				y = y - offset;
+			}
+			else if(direction == Direction.DOWN)
+			{
+				y = y + offset;
+			}
+		}
+
 		public bool IsHit(Point p)
 		{
 			return p.x == this.x && p.y == this.y;
 		}
 
-		public void Move(int offset, Direction direction)
-		{
-			if (direction == Direction.RIGHT) {
-				x = x + offset;
-			} else if (direction == Direction.LEFT) {
-				x = x - offset;
-			} else if (direction == Direction.UP) {
-				y = y - offset;
-			} else if (direction == Direction.DOWN) {
-				y = y + offset;
-			}
-
-		}
-
 		public void Draw()
 		{
-			Console.SetCursorPosition (x, y);
-			Console.Write (sym);
+			Console.SetCursorPosition( x, y );
+			Console.Write( sym );			
 		}
 
 		public void Clear()
 		{
 			sym = ' ';
-			Draw ();
+			Draw();
+		}
+
+		public override string ToString()
+		{
+			return x + ", " + y + ", " + sym;
 		}
 	}
 }
