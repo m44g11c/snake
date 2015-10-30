2e4cec633d425ff04358b156b367330fdbf306e6
m44g11c <abtoputet@tut.by>
2015-10-30 16:03:26 +0300

    Line

--- Snake/VerticalLine.cs
+++ Snake/VerticalLine.cs
@@ -1,20 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
 	class VerticalLine : Figure
 	{
 
-		public VerticalLine (int yTop, int yBot, int x, char sym)
-		{
-			pList = new System.Collections.Generic.List<Point> ();
-			for(int y = yTop; y <= yBot; y++)
+			public VerticalLine( int yUp, int yDown, int x, char sym )
 			{
-				Point p = new Point(x, y, sym);
-				pList.Add(p);
+				pList = new List<Point>();
+				for(int y = yUp; y <= yDown; y++)
+				{
+					Point p = new Point( x, y, sym );
+					pList.Add( p );
+				}			
 			}
-
-		}
 
 	}
 }
