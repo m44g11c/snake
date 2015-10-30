d9290add55c572c600bcd2c75ed734bc8f6fc305
m44g11c <abtoputet@tut.by>
2015-10-30 16:01:47 +0300

    Figure

--- Snake/Figure.cs
+++ Snake/Figure.cs
@@ -1,18 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
 	class Figure
 	{
-		
-		protected System.Collections.Generic.List<Point> pList;
+		protected List<Point> pList;
 
-		public void Drow ()
+		public void Draw()
 		{
-			foreach (Point p in pList)
+			foreach ( Point p in pList )
 			{
-				p.Draw ();
+				p.Draw();
 			}
+		}
+
+		internal bool IsHit( Figure figure )
+		{
+			foreach(var p in pList)
+			{
+				if ( figure.IsHit( p ) )
+					return true;
+			}
+			return false;
+		}
+
+		private bool IsHit( Point point )
+		{
+			foreach(var p in pList)
+			{
+				if ( p.IsHit( point ) )
+					return true;
+			}
+			return false;
 		}
 	}
 }
